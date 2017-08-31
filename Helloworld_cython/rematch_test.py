c5542a5b3239f7cd9b051023db46fa41ba2ef15d
YachaoShao <shaoyc429@163.com>
2017-08-31 13:22:17 +0200

    Run CYthon tuturio scipts.

--- Helloworld_cython/rematch_test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- UTF-8 -*-
-
-import re
-
-print (re.match('www','www.youtube.com'))
-print (re.match('com','www.youtube.com'))
-
--- Helloworld_cython/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-    ex_modules = cythonize("fibnacci.pyx")
-)
