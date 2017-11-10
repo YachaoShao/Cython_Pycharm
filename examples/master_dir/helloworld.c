48cfa7feb1791a0d7ea71cf89a845b3722f88ed7
Yachao Shao <y.shao@fz-juelich.de>
2017-11-10 11:01:04 +0100

    Merge branch 'test_cmake'
    # Conflicts:
    #	examples/master_dir/helloworld.c

--- examples/master_dir/helloworld.c
+++ examples/master_dir/helloworld.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
-
+//<<<<<<< HEAD
+//#include <stdio.h>
+//
+//int main(int argc, char* argv[]){
+//    printf("Hello world.\n");
+//=======
+#include <iostream>
 int main(int argc, char* argv[]){
-	printf("Hello world.\n");
+	std::cout << "Hello master:" << argv[1] <<std::endl;	
+//>>>>>>> test_cmake
 	return 0;
 }
